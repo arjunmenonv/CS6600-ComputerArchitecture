3b2d23b1b058f19ecb97c2ad0c0608c549312aa2
Akilesh Kannan <aklsh@tuta.io>
2021-09-16 22:03:54 +0530

    pushing for today

--- src/main.py
+++ src/main.py
@@ -1,19 +1,31 @@
 import argparse as ap
+from inc.opts import *
 from inc.parser import inputParser
 
 def args():
+    '''
+    Parse command-line arguments
+
+    '''
     parser = ap.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, metavar='file', help="Input file with requests", required=True)
-    arg = parser.parse_args()
-    return arg.input
-
+    args = parser.parse_args()
+    return args
 
 def main():
-    inFile = args()
+    '''
+    Main simulation function
+
+    '''
+    inFile = args().input
     fileParser = inputParser(inFile)
     reqs = fileParser.parse()
     for req in reqs:
         req.print()
+
+    kfree_pages = range(256)
+    ufree_pages = range(768)
+
 
 if __name__=="__main__":
     main()
--- src/mmu.py
+++
@@ -1,4 +0,0 @@
-class mmu:
-    def __init__(self):
-        self.kfree_pages = range(0,256)
-        self.ufree_pages = range(0,768)
