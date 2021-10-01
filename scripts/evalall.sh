da359aa78847696a28f2de85a5bdc06ef1845172
Akilesh Kannan <aklsh@tuta.io>
2021-10-01 20:49:00 +0530

    results and shenanigans

--- scripts/evalall.sh
+++ scripts/evalall.sh
@@ -14,10 +14,7 @@ run_suite=custom
 
 # The following line will launch sims for all workloads when you run ./doit.sh (comment it if you dont want it to)
 ##############################
-cd ../sim
-make clean
-make
-cd ../scripts
+
 time ./runall.pl -s ../sim/predictor -w $run_suite -f  $num_parallel_jobs -d ../results/TournamentBPUcustom
 ./getdata.pl -w $run_suite -noxxxx -d ../results/TournamentBPUcustom > ../results/TournamentBPUcustom/rollup
 ./getdata.pl -w $run_suite -s Predicted_Taken_Skew -noxxxx -d ../results/TournamentBPUcustom > ../results/TournamentBPUcustom/PTSresults
--- scripts/evaltemp.sh
+++ scripts/evaltemp.sh
@@ -14,10 +14,7 @@ run_suite=random
 
 # The following line will launch sims for all workloads when you run ./doit.sh (comment it if you dont want it to)
 ##############################
-cd ../sim
-make clean
-make
-cd ../scripts
+
 time ./runall.pl -s ../sim/predictor -w $run_suite -f  $num_parallel_jobs -d ../results/TournamentBPU
 ./getdata.pl -w $run_suite -noxxxx -d ../results/TournamentBPU > ../results/TournamentBPU/rollup
 ./getdata.pl -w $run_suite -s Predicted_Taken_Skew -noxxxx -d ../results/TournamentBPU > ../results/TournamentBPU/PTSresults
