2cc14b9873ba8886ecc7be0d6fbba1b478bcb1cb
Akilesh Kannan <aklsh@tuta.io>
2021-10-12 15:18:21 +0530

    finished register renaming except for reservation station forwarding

--- src/regfiles.py
+++ src/regfiles.py
@@ -1,15 +1,27 @@
+class arfEntry:
+    def __init__(self):
+        self.data = 0
+        self.busy = False
+        self.tag = 0
+    def print(self):
+        print("Data: {}\tBusy: {}\tTag: {}".format(self.data, self.busy, self.tag))
+
 class arf:
     def __init__(self, numEntries):
-        self.data = [None]*numEntries
-        self.busy = [False]*numEntries
-        self.tag = [None]*numEntries
+        self.entries = [arfEntry()]*numEntries
         print("Instantiated ARF")
+
+class rrfEntry:
+    def __init__(self):
+        self.data = 0
+        self.busy = False
+        self.valid = False
+    def print(self):
+        print("Data: {}\tBusy: {}\tValid: {}".format(self.data, self.busy, self.valid))
 
 class rrf:
     def __init__(self, numEntries):
-        self.data = [None]*numEntries
-        self.valid = [False]*numEntries
-        self.busy = [False]*numEntries
+        self.entries = [rrfEntry()]*numEntries
         print("Instantiated RRF")
 
 class regfiles:
@@ -17,3 +29,35 @@ class regfiles:
         self.rrf = rrf(numEntriesRRF)
         self.arf = arf(numEntriesARF)
         print("Regfiles instantiated")
+
+    def destinationAllocate(self, arfReg):
+        for index, entry in enumerate(self.rrf.entries):
+            if entry.busy == False:
+                entry.busy = True
+                entry.valid = False
+                self.arf.entries[arfReg].busy = True
+                self.arf.entries[arfReg].tag = index
+                break
+
+    def registerUpdate(self, rrfIndex, type, data):
+        if type=='finish':
+            self.rrf.entries[rrfIndex].data = data
+            self.rrf.entries[rrfIndex].valid = True
+        elif type=='complete':
+            for entry in self.arf.entries:
+                if entry.tag == rrfIndex:
+                    entry.data = self.rrf.entries[rrfIndex].data
+                    entry.busy = False
+                    self.rrf.entries[rrfIndex].busy = False
+                    break
+
+    def sourceRead(self, arfIndex):
+        if self.arf.entries[arfIndex].busy == False:
+            return self.arf.entries[arfIndex].data
+        else:
+            rrfIndex = self.arf.entries[arfIndex].tag
+            if self.rrf.entries[rrfIndex].valid == True:
+                return self.rrf.entries[rrfIndex].data
+            else: # forward tag to reservation station. How2???
+                print("Feature in development. Please wait")
+        return -1
