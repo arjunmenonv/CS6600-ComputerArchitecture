aa4114d2aa855a03b56694c21eb129124e438390
Arjun Menon Vadakkeveedu <>
2021-10-13 17:56:43 +0530

    Committing roBuffer, FU (wip) class definitions

--- src/dispatchBuffer.py
+++ src/dispatchBuffer.py
@@ -2,7 +2,14 @@ class dispatchBuffer:
     def __init__(self):
         print("Created")
 
-    def putIn(self): # play russian anthem xD
+    def putIn(self): # play russian anthem xD (ggwp)
         return
     def dispatch(self):
+        '''
+            Initiate reg renaming, source reading and allocating RS, ROBuffer entries
+            Stall if RRF, RS or ROBuffer is full
+
+            Assumption: Dispatch buffer can dispatch up to M (= dispatch width) instructions,
+            where dispatch width as defined in the config file
+        '''
         return
