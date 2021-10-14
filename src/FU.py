1ad0e975b65f7bfefceca324af6b1b81545c3f0c
Arjun Menon Vadakkeveedu <>
2021-10-14 22:53:33 +0530

    Separate classes for each FU

--- src/FU.py
+++ src/FU.py
@@ -1,14 +1,14 @@
-class FU:
-    def __init__(self, latency, type):
+class ASU:
+    '''
+        Add/Sub Functional Unit
+        Execution is pipelined with 1 instr fed in each cycle
+        latency of FU is as mentioned in config.py
+    '''
+    def __init__(self, latency):
         self.latency = latency
-        self.type = type
         '''
         InstrIdx => Reorder Buffer index, RegVal => value to be written to RenameReg & forwarded
-        opCode:
-            "add":  1 => add, 0 => sub
-            "mult": 1 => mult, 0 => div
-            "mem":  1 => load, 0 => store
-            when no instruction is issued by RS, set opCode = -1
+        opCode: 1 => add, 0 => sub
         '''
         self.dict = [{'InstrIdx': -1, 'RegVal': None, 'opCode': -1}]*self.latency
 
@@ -22,22 +22,75 @@ class FU:
         if (opCode == -1):      # NOP bubble
             regval = None
         else:
-            if (self.type == "add"):
-                if opCode:
-                    regval = op1 + op2
-                else:
-                    regval = op1 - op2
-            elif (self.type == "mult"):
-                if opCode:
-                    regval = op1*op2
-                else:
-                    regval = op1/op2
-            else:       # mem operation
-                if opCode:
-                    #regval = implement load operation here
-                    regval = 0      # remove this
-                else:
-                    regval = None
-                    # implement store operation here
+            if opCode:
+                regval = op1 + op2
+            else:
+                regval = op1 - op2
+        self.dict[0] = {'InstrIdx': index, 'RegVal': regval, 'opCode': opCode}
+        return out
+
+class MDU:
+    '''
+        Mult/Div Functional Unit
+        Execution is pipelined with 1 instr fed in each cycle
+        latency of FU is as mentioned in config.py
+    '''
+    def __init__(self, latency):
+        self.latency = latency
+        '''
+        InstrIdx => Reorder Buffer index, RegVal => value to be written to RenameReg & forwarded
+        opCode: 1 => mult, 0 => div
+        '''
+        self.dict = [{'InstrIdx': -1, 'RegVal': None, 'opCode': -1}]*self.latency
+
+    def shiftAndEval(self, index= -1, opCode= -1, op1=None, op2=None):
+        '''
+        Note: If the RS is full, issue a NOP (so that the output value is got)
+              by passing the default values to shiftAndEval()
+        '''
+        out = self.dict[-1]
+        self.dict[1:] = self.dict[0:-1]     # shift (pipelined exec)
+        if (opCode == -1):      # NOP bubble
+            regval = None
+        else:
+            if opCode:
+                regval = op1*op2
+            else:
+                regval = op1/op2
+        self.dict[0] = {'InstrIdx': index, 'RegVal': regval, 'opCode': opCode}
+        return out
+
+class LSU:
+    '''
+        Load/Store Functional Unit
+        Execution is pipelined with 1 instr fed in each cycle
+        latency of FU is as mentioned in config.py
+    '''
+    def __init__(self, latency):
+        self.latency = latency
+        '''
+        InstrIdx => Reorder Buffer index, RegVal => value to be written to RenameReg & forwarded
+        opCode: 1 => load, 0 => store
+        '''
+        self.dict = [{'InstrIdx': -1, 'RegVal': None, 'opCode': -1}]*self.latency
+
+    def shiftAndEval(self, index= -1, opCode= -1, op1=None, op2=None, offset= None):
+        '''
+        Note: If the RS is full, issue a NOP (so that the output value is got)
+              by passing the default values to shiftAndEval()
+        '''
+        out = self.dict[-1]
+        self.dict[1:] = self.dict[0:-1]     # shift (pipelined exec)
+        if (opCode == -1):      # NOP bubble
+            regval = None
+        else:
+            if opCode:
+                addr = (op1 + op2)%len(mem)
+                regval = mem[addr]
+            else:
+                regval = None
+                addr = (op2 + offset)%len(mem)
+                mem[addr] = op1
+                # Implement Store Operation here
         self.dict[0] = {'InstrIdx': index, 'RegVal': regval, 'opCode': opCode}
         return out
