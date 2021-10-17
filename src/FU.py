1cb0458e3c868541d614d532beb791db723df39d
Arjun Menon Vadakkeveedu <>
2021-10-17 17:20:54 +0530

    Changed FU for Load/Store

--- src/FU.py
+++ src/FU.py
@@ -113,34 +113,44 @@ class DU:
 class LSU:
     '''
         Load/Store Functional Unit
-        Execution is pipelined with 1 instr fed in each cycle
-        latency of FU is as mentioned in config.py
+        Unlike othger FUs, the LSU is not pipelined
     '''
     def __init__(self, latency):
         self.latency = latency
-        '''
-        InstrIdx => Reorder Buffer index, RegVal => value to be written to RenameReg & forwarded
-        opCode: 1 => load, 0 => store
-        '''
-        self.dict = [{'InstrIdx': -1, 'RegVal': None, 'opCode': -1}]*self.latency
+        self.dict = {'InstrIdx': -1, 'RegVal': None, 'opCode': -1, 'busy': 0}
+        self.end = None
 
-    def shiftAndEval(self, index= -1, opCode= -1, op1=None, op2=None, offset= None):
+    def IssueNewOp(self, start, index= -1, opCode= -1, op1=None, op2=None, offset= None):
         '''
-        Note: If the RS is full, issue a NOP (so that the output value is got)
-              by passing the default values to shiftAndEval()
+            Check if FU is busy before calling this Function
+            Redundant "if(busy)" added
         '''
-        out = self.dict[-1]
-        self.dict[1:] = self.dict[0:-1]     # shift (pipelined exec)
-        if (opCode == -1):      # NOP bubble
-            regval = None
+        if (self.dict['busy']):
+            return 1       # busy, try again in next cycle
         else:
-            if opCode:
-                addr = (op1 + op2)%len(mem)
-                regval = mem[addr]
-            else:
+            if (opCode == -1):      # NOP bubble
                 regval = None
-                addr = (op2 + offset)%len(mem)
-                mem[addr] = op1
-                # Implement Store Operation here
-        self.dict[0] = {'InstrIdx': index, 'RegVal': regval, 'opCode': opCode}
-        return out
+                busy = 0
+            else:
+                busy = 1
+                self.end = start + self.latency
+                if opCode == 'LOD':
+                    addr = (op1 + op2)%len(mem)
+                    regval = mem[addr]
+                elif opCode == 'STO':
+                    regval = None
+                    addr = (op2 + offset)%len(mem)
+                    mem[addr] = op1
+                else:
+                    regval = None
+                    index = -1
+                    busy = 0
+            self.dict = {'InstrIdx': index, 'RegVal': regval, 'opCode': opCode, 'busy': busy}
+            return 0
+
+    def pollLSU(self, clkVal):
+        if(self.dict['busy']):
+            if(clkVal == self.end):
+                self.dict['busy'] = 0
+                return self.dict
+        return None
